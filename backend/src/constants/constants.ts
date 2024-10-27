db42bc054865882bd4c570cc062f51a658400aa9
Nikos Koumbakis <spiralout.eu@gmail.com>
2024-10-27 15:46:29 +0200

    fix strings

--- backend/src/constants/constants.ts
+++ backend/src/constants/constants.ts
@@ -1,17 +1,17 @@
 // Used for `ntt add-chain ${chainString}`
 export const ADD_CHAIN_ID_MAP: { [key: string]: string } = {
-  '1': 'ethereum',
-  '11155111': 'sepolia',
-  '137': 'polygon',
-  '42161': 'arbitrum',
-  '421614': 'arbitrumSepolia',
-  '8453': 'base',
-  '84532': 'baseSepolia',
-  '10': 'optimism',
-  '11155420': 'optimismSepolia',
-  '901': 'solana', // TODO: check chain ID solana devnet
-  '101': 'sui',
-  '102': 'sui', // TODO: check chain ID sui devnet
-  '103': 'sui', // TODO: check chain ID sui testnet
+  '1': 'Ethereum',
+  '11155111': 'Sepolia',
+  '137': 'Polygon',
+  '42161': 'Arbitrum',
+  '421614': 'ArbitrumSepolia',
+  '8453': 'Base',
+  '84532': 'BaseSepolia',
+  '10': 'Optimism',
+  '11155420': 'OptimismSepolia',
+  '901': 'Solana', // TODO: check chain ID solana devnet
+  '101': 'Sui',
+  '102': 'Sui', // TODO: check chain ID sui devnet
+  '103': 'Sui', // TODO: check chain ID sui testnet
   // Add other mappings as necessary
 };
